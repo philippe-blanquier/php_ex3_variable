9bad9e36847be4223daaa3b0adcaf0353dd57706
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 10:39:08 +0200

    creation

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex3</title>
+    </head>
+    <body >
+        <?php
+        $km=1;
+        echo nl2br("PHP Wellcome exercise 2\nkm=".$km);
+        $km=3;
+        echo nl2br("\nkm=".$km);
+        $km=25;
+        echo nl2br("\nkm=".$km);
+        ?>
+    </body>
+</html>
