9385f7585600661372d613ef7d6940466d94de6d
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:12:57 +0200

    evo

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-        <title>PHP ex3</title>
+        <title>PHP ex1.3</title>
     </head>
     <body >
-        <?php
-        $km=1;
-        echo nl2br("PHP Wellcome exercise 2\nkm=".$km);
-        $km=3;
-        echo nl2br("\nkm=".$km);
-        $km=25;
-        echo nl2br("\nkm=".$km);
-        ?>
+        <p>
+            <?php
+            $km=1;
+            echo nl2br("PHP Wellcome exercise 2\nkm=".$km);
+            $km=3;
+            echo nl2br("\nkm=".$km);
+            $km=25;
+            echo nl2br("\nkm=".$km);
+            ?>
+        </p>
     </body>
 </html>
